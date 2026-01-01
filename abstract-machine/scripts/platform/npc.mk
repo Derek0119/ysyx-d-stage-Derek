d854331fdd8862b94f6cc2040ec39b4b79b0f3a5
Derek0119 <2476840679@qq.com>
2026-01-01 22:37:39 +0800

    第6次ci测试

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -23,11 +23,36 @@ CFLAGS += -I$(AM_HOME)/am/src/riscv/npc/include #$(shell pkg-config --cflags sdl
 insert-arg: image
 	@python $(AM_HOME)/tools/insert-arg.py $(IMAGE).bin $(MAINARGS_MAX_LEN) $(MAINARGS_PLACEHOLDER) "$(mainargs)"
 
-image: image-dep
-	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
-	@echo + OBJCOPY "->" $(IMAGE_REL).bin
-	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
-# 	@$(OBJCOPY) -I binary -O verilog --adjust-vma=$(LOAD_ADDR) $(IMAGE).bin
+
+
+ELF_OFFSET := 370432
+
+# 源模板文件（对应你 gen.sh 里的 HELLO_BIN）
+HELLO_TEMPLATE := $(YSYX_HOME)/ysyxSoC/ready-to-run/D-stage/hello-minirv-ysyxsoc.bin
+
+# 最终要生成的 bin
+IMAGE_BIN := $(IMAGE).bin
+
+# 真正的依赖：elf 文件变了就要重新 patch
+$(IMAGE_BIN): $(IMAGE).elf $(HELLO_TEMPLATE)
+	@echo "  PATCH  $@"
+	@cp $(HELLO_TEMPLATE) $@.tmp
+	@dd if=$< of=$@.tmp bs=1 seek=$(ELF_OFFSET) conv=notrunc 2>/dev/null
+	@mv $@.tmp $@
+
+# 如果你还想保留反汇编，可以保留原来的 image 目标
+image: $(IMAGE_BIN)
+	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).tx
+
+
+
+# image: image-dep
+# 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
+# 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
+# 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
+# # 	@$(OBJCOPY) -I binary -O verilog --adjust-vma=$(LOAD_ADDR) $(IMAGE).bin $(IMAGE).hex
+
+
 run: insert-arg
 	echo "TODO: add command here to run simulation"
 
